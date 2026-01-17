1b3dc58cfcc861197d6f63e1749de625f98227e0
Sunee <897745014@qq.com>
2026-01-18 00:07:52 +0800

    add player and animation

--- Player/player.gd
+++ Player/player.gd
@@ -5,18 +5,19 @@ const ACCELERATION = 600.0
 const FRICTION = 1000.0
 
 @onready var animationPlayer = $AnimationPlayer
+@onready var animationTree = $AnimationTree
 
 func _physics_process(delta: float) -> void:
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if input_dir != Vector2.ZERO:
-		if input_dir.x > 0:
-			animationPlayer.play("RunRight")
-		else:
-			animationPlayer.play("RunLeft")
+		animationTree.set("parameters/Idel/blend_position", input_dir)
+		animationTree.set("parameters/Run/blend_position", input_dir)
 		var target_velocity = input_dir * MAX_SPEED
 		velocity = velocity.move_toward(target_velocity, ACCELERATION * delta)
 	else:
-		animationPlayer.play("IdelRight")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	velocity = velocity.limit_length(MAX_SPEED)
 	move_and_slide()
+
+func _process(_delta):
+	global_position = global_position.snapped(Vector2(1, 1))

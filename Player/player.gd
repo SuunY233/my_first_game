b9e2b7929300b2c4df3cc354fe71f4608be6b0b4
Sunee <897745014@qq.com>
2026-01-19 23:02:35 +0800

    modify animationTree

--- Player/player.gd
+++ Player/player.gd
@@ -4,18 +4,18 @@ const MAX_SPEED = 150.0
 const ACCELERATION = 600.0
 const FRICTION = 1000.0
 
+var input_dir = Vector2.ZERO
 @onready var animationTree = $AnimationTree
 @onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta: float) -> void:
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if input_dir != Vector2.ZERO:
-		animationTree.set("parameters/Run/blend_position", input_dir)
+		update_blend_position(input_dir)
 		animationState.travel("Run")
 		var target_velocity = input_dir * MAX_SPEED
 		velocity = velocity.move_toward(target_velocity, ACCELERATION * delta)
 	else:
-		animationTree.set("parameters/Idle/blend_position", input_dir)
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	velocity = velocity.limit_length(MAX_SPEED)
@@ -23,3 +23,7 @@ func _physics_process(delta: float) -> void:
 
 func _process(_delta):
 	global_position = global_position.snapped(Vector2.ONE)
+
+func update_blend_position(input_dir: Vector2) -> void:
+	animationTree.set("parameters/Run/blend_position", input_dir)
+	animationTree.set("parameters/Idle/blend_position", input_dir)

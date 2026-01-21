f4fc5bc33cee1221593000319a0f8f20fb793887
Sunee <897745014@qq.com>
2026-01-21 22:31:24 +0800

    add attack and roll

--- Player/player.gd
+++ Player/player.gd
@@ -6,20 +6,24 @@ const FRICTION = 1000.0
 
 var input_dir = Vector2.ZERO
 @onready var animationTree = $AnimationTree
-@onready var animationState = animationTree.get("parameters/playback")
+@onready var animationState = animationTree.get("parameters/playback") as AnimationNodeStateMachinePlayback
 
 func _physics_process(delta: float) -> void:
-	input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	if input_dir != Vector2.ZERO:
-		update_blend_position(input_dir)
-		animationState.travel("Run")
-		var target_velocity = input_dir * MAX_SPEED
-		velocity = velocity.move_toward(target_velocity, ACCELERATION * delta)
-	else:
-		animationState.travel("Idle")
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-	velocity = velocity.limit_length(MAX_SPEED)
-	move_and_slide()
+	var state = animationState.get_current_node()
+	if state == "Run" or state == "Idle":
+		input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+		if input_dir != Vector2.ZERO:
+			update_blend_position(input_dir)
+			animationState.travel("Run")
+			var target_velocity = input_dir * MAX_SPEED
+			velocity = velocity.move_toward(target_velocity, ACCELERATION * delta)
+		else:
+			animationState.travel("Idle")
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		velocity = velocity.limit_length(MAX_SPEED)
+		move_and_slide()
+	elif state == "Attack":
+		pass
 
 func _process(_delta):
 	global_position = global_position.snapped(Vector2.ONE)
@@ -27,3 +31,5 @@ func _process(_delta):
 func update_blend_position(input_dir: Vector2) -> void:
 	animationTree.set("parameters/Run/blend_position", input_dir)
 	animationTree.set("parameters/Idle/blend_position", input_dir)
+	animationTree.set("parameters/Attack/blend_position", input_dir)
+	animationTree.set("parameters/Rool/blend_position", input_dir)

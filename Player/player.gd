8747a47b48d3ab6671b363455a0083d4fd119bce
Sunee <897745014@qq.com>
2026-01-23 23:43:58 +0800

    modify world

--- Player/player.gd
+++ Player/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const MAX_SPEED = 150.0
+const MAX_SPEED = 100.0
 const ACCELERATION = 600.0
 const FRICTION = 1000.0
 
@@ -9,21 +9,17 @@ var input_dir = Vector2.ZERO
 @onready var animationState = animationTree.get("parameters/playback") as AnimationNodeStateMachinePlayback
 
 func _physics_process(delta: float) -> void:
-	var state = animationState.get_current_node()
-	if state == "Run" or state == "Idle":
-		input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-		if input_dir != Vector2.ZERO:
-			update_blend_position(input_dir)
-			animationState.travel("Run")
-			var target_velocity = input_dir * MAX_SPEED
-			velocity = velocity.move_toward(target_velocity, ACCELERATION * delta)
-		else:
-			animationState.travel("Idle")
-			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-		velocity = velocity.limit_length(MAX_SPEED)
-		move_and_slide()
-	elif state == "Attack":
-		pass
+	input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	if input_dir != Vector2.ZERO:
+		update_blend_position(input_dir)
+		animationState.travel("Run")
+		var target_velocity = input_dir * MAX_SPEED
+		velocity = velocity.move_toward(target_velocity, ACCELERATION * delta)
+	else:
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	velocity = velocity.limit_length(MAX_SPEED)
+	move_and_slide()
 
 func _process(_delta):
 	global_position = global_position.snapped(Vector2.ONE)

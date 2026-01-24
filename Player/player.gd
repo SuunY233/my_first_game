c635b9e310362778156bf8c4647f0ddcd7b8c31c
Sunee <897745014@qq.com>
2026-01-24 19:58:37 +0800

    add roll and attack

--- Player/player.gd
+++ Player/player.gd
@@ -1,31 +1,42 @@
 extends CharacterBody2D
 
-const MAX_SPEED = 100.0
-const ACCELERATION = 600.0
-const FRICTION = 1000.0
+const SPEED = 120.0
+const ROLL_SPEED = 125.0
 
 var input_dir = Vector2.ZERO
-@onready var animationTree = $AnimationTree
-@onready var animationState = animationTree.get("parameters/playback") as AnimationNodeStateMachinePlayback
+var last_input_dir = Vector2.ZERO
+
+@onready var animationTree: AnimationTree = $AnimationTree
+@onready var playback = animationTree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback
 
 func _physics_process(delta: float) -> void:
+	var state = playback.get_current_node()
+	match state:
+		"MoveState":
+			process_move_state(delta)
+		"AttackState":
+			pass
+		"RollState":
+			process_roll_state(delta)
+
+func process_move_state(delta: float) -> void:
 	input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if input_dir != Vector2.ZERO:
+		last_input_dir = input_dir
 		update_blend_position(input_dir)
-		animationState.travel("Run")
-		var target_velocity = input_dir * MAX_SPEED
-		velocity = velocity.move_toward(target_velocity, ACCELERATION * delta)
-	else:
-		animationState.travel("Idle")
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-	velocity = velocity.limit_length(MAX_SPEED)
+	if Input.is_action_just_pressed("attack"):
+		playback.travel("AttackState")
+	if Input.is_action_just_pressed("roll"):
+		playback.travel("RollState")
+	velocity = input_dir * SPEED
 	move_and_slide()
 
-func _process(_delta):
-	global_position = global_position.snapped(Vector2.ONE)
+func process_roll_state(delta: float) -> void:
+	velocity = last_input_dir * ROLL_SPEED
+	move_and_slide()
 
 func update_blend_position(input_dir: Vector2) -> void:
-	animationTree.set("parameters/Run/blend_position", input_dir)
-	animationTree.set("parameters/Idle/blend_position", input_dir)
-	animationTree.set("parameters/Attack/blend_position", input_dir)
-	animationTree.set("parameters/Rool/blend_position", input_dir)
+	animationTree.set("parameters/StateMachine/MoveState/RunState/blend_position", input_dir)
+	animationTree.set("parameters/StateMachine/MoveState/IdleState/blend_position", input_dir)
+	animationTree.set("parameters/StateMachine/AttackState/blend_position", input_dir)
+	animationTree.set("parameters/StateMachine/RollState/blend_position", input_dir)
